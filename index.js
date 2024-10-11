4ec976a123fd9fc5a9ed4fa116545c64df5d3102
demuelle <dmueller@trilogyed.com>
2024-10-11 14:06:22 -0500

    Changes name of sample output flie

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type: "input",
+  name: "title",
+  message: "What is your project called?"
+},
+{
+  type: "input",
+  name: "description",
+  message: "Describe the project"
+},// and more!!!
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // seems like it should call fs.writeFile
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // this will probably call inquirer.prompt(...).then(...)  
+
+}
 
 // Function call to initialize app
 init();
